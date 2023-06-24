b51ea184e76a19574a2dc82bc1f05aec4b83d2a8
Montes-Tobar <mt17005@ues.edu.sv>
2023-06-24 10:00:55 -0600

    config-service update

--- config-service/src/main/java/com/config/service/ConfigServiceApplication.java
+++ config-service/src/main/java/com/config/service/ConfigServiceApplication.java
@@ -2,8 +2,11 @@ package com.config.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+//Anotación para habilitar el config-server de Github
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
