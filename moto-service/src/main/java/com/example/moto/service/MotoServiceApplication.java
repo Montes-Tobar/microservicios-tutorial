c1c67c2f93d59e62c3dd75086780f57d72669eb7
Montes-Tobar <mt17005@ues.edu.sv>
2023-06-25 17:05:52 -0600

    Configuración Eureka

--- moto-service/src/main/java/com/example/moto/service/MotoServiceApplication.java
+++ moto-service/src/main/java/com/example/moto/service/MotoServiceApplication.java
@@ -2,8 +2,13 @@ package com.example.moto.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+
+//Anotación de Eureka Client
+@EnableEurekaClient
+
 public class MotoServiceApplication {
 
 	public static void main(String[] args) {
--- usuario-service/src/main/java/com/example/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/example/usuario/service/UsuarioServiceApplication.java
@@ -2,11 +2,14 @@ package com.example.usuario.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //Anotación de Feign
 @EnableFeignClients
 @SpringBootApplication
+//Anotación de Eureka Client
+@EnableEurekaClient
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
